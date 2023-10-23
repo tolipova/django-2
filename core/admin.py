d247859ddc8f5fae0fe5952f845f1a0400636017
tolipova <shahinakhan3005@gmail.com>
2023-10-23 13:25:27 +0300

    media

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Malumot
+from django.utils.html import format_html
+from django.utils.html import mark_safe
 # Register your models here.
-admin.site.register(Malumot)
+class InformationAdmin(admin.ModelAdmin):
+    list_display = ('ism','about','age','get_html_photo',)
+    list_filter = ('ism',)
+
+    def get_html_photo(self,object):
+        if object.image:
+            return mark_safe(f'<img src="{object.image.url}" width="50">')
+        return None
+    get_html_photo.short_description = 'Surat'
+admin.site.register(Malumot, InformationAdmin)
--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class Malumot(models.Model):
     ism = models.CharField(max_length=255)
     about = models.TextField()
     age = models.IntegerField()
+    image = models.ImageField(upload_to='user_info/')
     
     def __str__(self):
         return self.ism
